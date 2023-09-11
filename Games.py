7df11518419c257a929b762e5ca65b8401c3a31b
Tyron Bechayda <tyronbechayda1112@gmail.com>
2023-09-11 22:03:44 +0800

    Hangman Games program

---
+++ Games.py
@@ -0,0 +1,116 @@
+import random
+import os
+
+class Hangman(object):
+    HANGINGMAN = [
+        '''
+        +---+
+        |   |
+        |   
+        |
+        |
+        [========]''',
+        '''
+        +---+
+        |   |
+        |   O
+        |
+        |
+        [========]''',
+        '''
+        +---+
+        |   |
+        |   O
+        |   |
+        |
+        [========]''',
+        '''
+        +---+
+        |   |
+        |   O
+        |  /|
+        |
+        [========]''',
+        '''
+        +---+
+        |   |
+        |   O
+        |  /|\\
+        |
+        [========]''',
+        '''
+        +---+
+        |   |
+        |   O
+        |  /|\\
+        |  /
+        [========]''',
+        '''
+        +---+
+        |   |
+        |   O
+        |  /|\\
+        |  / \\
+        [========]'''
+    ]
+    WORDBANK = ['HAPPY', 'BIRTHDAY', 'HYDROPHOBIC', 'COMPUTER', 'MOUSE', 'LAPTOP', 'PILLOWS',
+                'SNOWMAN', 'ABOVE', 'ABOUT', 'AFTER', 'AGAIN', 'AGREE', 'ALL', 'ALONE',
+                'ALONG', 'ALMOST', 'ALSO', 'AM', 'AN', 'AND', 'ANY', 'ARE', 'AS', 'AT', 'AWAY']
+    def __init__(self):
+        self.Word_to_Guess = random.choice(self.WORDBANK)
+        self.Gameplay()
+    def Dict_word(self, GOTWORD):
+        dict_count = dict()
+        for index, charac in enumerate(GOTWORD):
+            if charac in dict_count:
+                dict_count[charac].append(index)
+            else:
+                dict_count[charac] = [index]
+        return dict_count
+    def Check_guess(self, underscores, guess, dict_coll):
+        list_underscores = [char for char in underscores]
+        if guess.isdigit():
+            print('The entered guess was a digit')
+            return (underscores, False)
+        elif len(guess) == 1 and guess in dict_coll:
+            for index in dict_coll[guess]:
+                list_underscores[index] = guess
+            underscores = ''.join(list_underscores)
+            return (underscores, True)
+        elif len(guess) > 1:
+            return (guess, True)
+        elif guess in underscores:
+            print("The letter is already been selected")
+            return (underscores, False)
+        else:
+            if len(guess) == 1:
+                print('The guessed letter does not exist in the word')
+            else:
+                print('The guessed word is wrong')
+            return (underscores, False)
+    def Gameplay(self):
+        print('HANGMAN GAME!!!')
+        i = 0
+        underscore_word = '_' * len(self.Word_to_Guess)
+        char_locations = self.Dict_word(self.Word_to_Guess)
+        while True:
+            os.system('cls' if os.name == 'nt' else 'clear')  # Clear the console screen
+            print(self.HANGINGMAN[i])
+            print()
+            print('\t' + underscore_word)
+            print()
+            user_guess = input('Enter a letter or word if you know the answer: ')
+            underscore_word, flag = self.Check_guess(underscore_word, user_guess.upper(), char_locations)
+            if not flag:
+                i += 1
+            elif underscore_word == self.Word_to_Guess:
+                print(underscore_word)
+                print('Congratulations! You guessed the word and\nsaved the man')
+                break
+            if i == len(self.HANGINGMAN):
+                print('Sorry, you lose the game')
+                break
+            input('Press Enter to continue...')
+
+if __name__ == '__main__':
+    Game = Hangman()
